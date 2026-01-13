6c141fcb46f61b169543c27306441a0d2c6e73ff
ayrinhaha <irenemae.pingol@lorma.edu>
2026-01-13 12:28:29 +0800

    Added Array1.java

--- Array1.java
+++ Array1.java
@@ -1,7 +1,22 @@
-public class Array1{
-public static void main(String[] args){
-    int[] arr = {2, 2, 5, 3, 5, 6, 6, 8, 3, 10};
-    for (int i = 0; i < arr.length; i++)
-        System.out.println(arr[i]);
+import java.util.Scanner;
+
+public class Array1 {
+    public static void main(String[] args) {
+
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int index = -1;
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] == n) {
+                index = i;
+                break;
+            }
+            System.out.println("Index of " + n + " is " + index);
     }
+
 }
