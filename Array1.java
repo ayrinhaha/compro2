b4f220bfb2729c428d3187edc955f2fcab955c5c
ayrinhaha <irenemae.pingol@lorma.edu>
2026-01-13 11:53:22 +0800

    Added Array1.java

---
+++ Array1.java
@@ -0,0 +1,7 @@
+public class Array1{
+public static void main(String[] args){
+    int[] arr = {2, 2, 5, 3, 5, 6, 6, 8, 3, 10};
+    for (int i = 0; i < arr.length; i++)
+        System.out.println(arr[i]);
+    }
+}
