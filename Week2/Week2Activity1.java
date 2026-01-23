12b4341b104212327273e509e736d487a4e58469
ayayrenn <irenemaepingol.07@gmail.com>
2026-01-23 20:55:01 +0800

    added final act1 with random available seat

--- Week2/Week2Activity1.java
+++ Week2/Week2Activity1.java
@@ -1,5 +1,7 @@
 package Week2;
 
+import java.util.*;
+
 public class Week2Activity1 {
 
     public static void main(String[] args) {
@@ -54,5 +56,36 @@ public class Week2Activity1 {
 
         System.out.println("\nTotal booked seats: " + bookedSeatCount);
         System.out.println("Total available seats: " + notBookedSeatCount);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("\nDo you want to book a random available seat? (Y/N): ");
+        char choice = sc.next().charAt(0);
+
+        if (choice == 'Y' || choice == 'y') {
+            boolean booked = false;
+
+            while (!booked) {
+                int randomRow = (int) (Math.random() * 5);  // 0-4
+                int randomCol = (int) (Math.random() * 8);  // 0-7
+
+                if (theater[randomRow][randomCol] == 0) {
+                    theater[randomRow][randomCol] = 1;
+                    System.out.println("Seat booked at row " + randomRow + ", column " + randomCol);
+                    booked = true;
+                }
+            }
+
+            // Print updated seating chart
+            System.out.println("\n Updated Theater Seating Chart");
+            for (int i = 0; i < theater.length; i++) {
+                for (int j = 0; j < theater[i].length; j++) {
+                    System.out.print((theater[i][j] == 1 ? "[x]" : "[-]") + " ");
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("No seat booked.");
+        }
     }
 }
