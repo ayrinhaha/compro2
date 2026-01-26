7474fedbe7f2d68ed850ec7a6352ab33f7dc8a19
ayrinhaha <irenemae.pingol@lorma.edu>
2026-01-26 11:14:09 +0800

    organized folder

--- Week2/activities/Week2Acitvity1.java
+++ Week2/activities/Week2Acitvity1.java
@@ -1,4 +1,4 @@
-package Week2;
+package Week2.Activities;
 
 public class Week2Acitvity1 {
     public static void main(String[] args) {
--- Week2/practice/TwoDimensionalArray.java
+++ Week2/practice/TwoDimensionalArray.java
@@ -1,4 +1,4 @@
-package Week2;
+package Week2.practice;
 
 public class TwoDimensionalArray {
     public static void main(String[] args) {
