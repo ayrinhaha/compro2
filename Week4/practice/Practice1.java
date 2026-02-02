edcd9984232e046fc723f0dfc2216193a421d88e
ayrinhaha <irenemae.pingol@lorma.edu>
2026-02-02 12:33:10 +0800

    added week4 prac1

---
+++ Week4/practice/Practice1.java
@@ -0,0 +1,45 @@
+package Week4.practice;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+
+        try (Scanner sc = new Scanner(System.in)) {
+            sb.append("First Name: ");
+            System.out.print("First Name: ");
+
+            sb.append(sc.nextLine()).append("\nLast Name: ");
+            System.out.print("Last Name: ");
+
+            sb.append(sc.nextLine()).append("\nAge: ");
+            System.out.print("Age: ");
+
+            sb.append(sc.nextLine()).append("\nEmail: ");
+            System.out.print("Email: ");
+
+            sb.append(sc.nextLine()).append("\nPhone: ");
+            System.out.print("Phone:");
+
+            sb.append(sc.nextLine());
+        }
+
+        catch (InputMismatchException e) {
+            System.out.println("Invalid input");
+        }
+
+        
+
+        // try-with-resource
+        try (FileWriter fw = new FileWriter("data.txt")) {
+            fw.write(sb.toString());
+            System.out.println("Data is saved...");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
